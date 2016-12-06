7aeca0f14b7b2df047d68193d82a11a43ebf3ac0
coderick14 <bhattacharyya.rick14@gmail.com>
2016-12-06 22:54:53 +0530

    Change sessionId to string

--- migrations/20161206145129_create_sessions_table.up.sql
+++ migrations/20161206145129_create_sessions_table.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS Sessions (
-	sessionId int(11) NOT NULL AUTO_INCREMENT,
+	sessionId varchar(255) NOT NULL,
 	`key` varchar(255) NOT NULL,
 	value varchar(255) NOT NULL,
 	PRIMARY KEY (sessionId)
-) AUTO_INCREMENT=1;
+);
