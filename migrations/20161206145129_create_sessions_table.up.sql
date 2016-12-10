fc8c64553779c83862e719bf118b062e0c690d40
Parth Thakkar <thakkarparth007@gmail.com>
2016-12-10 22:52:46 +0530

    Fix primary key in Sessions table

--- migrations/20161206145129_create_sessions_table.up.sql
+++ migrations/20161206145129_create_sessions_table.up.sql
@@ -2,5 +2,5 @@ CREATE TABLE IF NOT EXISTS Sessions (
 	sessionId varchar(255) NOT NULL,
 	`key` varchar(255) NOT NULL,
 	value varchar(255) NOT NULL,
-	PRIMARY KEY (sessionId)
+	PRIMARY KEY (sessionId, `key`)
 );
