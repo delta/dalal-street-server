12bb9dde6a366da60b6668c2da46ac3a9303cda3
Rahul Zoldyck <rahulzoldyck@gmail.com>
2017-01-10 21:10:54 +0530

    Sockets finished
    Sockets is over except integration with protobuffer

--- models/Ask.go
+++ models/Ask.go
@@ -19,16 +19,16 @@ func (ot OrderType) String() string {
 }
 
 type Ask struct {
-	Id                     uint32       `gorm:"primary_key;AUTO_INCREMENT"`
-	UserId                 uint32       `gorm:"column:userId;not null"`
-	StockId                uint32       `gorm:"column:stockId;not null"`
-	OrderType              OrderType    `gorm:"column:orderType;not null"`
-	Price                  uint32       `gorm:"not null"`
-	StockQuantity          uint32       `gorm:"column:stockQuantity;not null"`
-	StockQuantityFulFilled uint32       `gorm:"column:stockQuantityFulFilled;not null"`
-	IsClosed               uint8        `gorm:"column:isClosed;not null"`
-	CreatedAt              string       `gorm:"column:createdAt;not null"`
-	UpdatedAt              string       `gorm:"column:updatedAt;not null"`
+	Id                     uint32    `gorm:"primary_key;AUTO_INCREMENT"`
+	UserId                 uint32    `gorm:"column:userId;not null"`
+	StockId                uint32    `gorm:"column:stockId;not null"`
+	OrderType              OrderType `gorm:"column:orderType;not null"`
+	Price                  uint32    `gorm:"not null"`
+	StockQuantity          uint32    `gorm:"column:stockQuantity;not null"`
+	StockQuantityFulFilled uint32    `gorm:"column:stockQuantityFulFilled;not null"`
+	IsClosed               uint8     `gorm:"column:isClosed;not null"`
+	CreatedAt              string    `gorm:"column:createdAt;not null"`
+	UpdatedAt              string    `gorm:"column:updatedAt;not null"`
 }
 
 func (Ask) TableName() string {
