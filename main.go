615572869513418612adfc8bfa0e31eacd77781e
Rahul Zoldyck <rahulzoldyck@gmail.com>
2017-01-11 19:58:39 +0530

    Formatted with gofmt

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
+
 // This main is for testing sockets only so ignore it
-import(
+import (
 	"./connections"
 	"log"
 )
@@ -8,13 +9,14 @@ import(
 func main() {
 	go func() {
 		log.Print("starting go loop")
-		s := <- connections.Test
+		s := <-connections.Test
 		log.Print("ending go loop")
-		connections.SendMessage(zold{ Name: "rahul"}, 1)
+		connections.SendMessage(zold{Name: "rahul"}, 1)
 		log.Print(s)
 	}()
 	connections.WebsocketServerInit()
 }
+
 type zold struct {
 	Name string
 }
