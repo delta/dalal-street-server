938f501d362820da1f2db380c01bc8d6d7d491a3
Parth Thakkar <thakkarparth007@gmail.com>
2017-01-28 03:14:07 +0530

    Integrate Pragyan API for Login

---
+++ models/Db.go
@@ -0,0 +1,20 @@
+// Package models handles everything between the database and the API.
+// All business logic is written in this package, so the user of this
+// package does not need to take care of race conditions in updating
+// the data.
+package models
+
+import (
+	"github.com/Sirupsen/logrus"
+	"github.com/thakkarparth007/dalal-street-server/utils"
+)
+
+var logger *logrus.Entry
+var DbOpen = utils.DbOpen
+
+func InitModels() {
+	logger = utils.Logger.WithFields(logrus.Fields{
+		"module": "models",
+	})
+}
+
