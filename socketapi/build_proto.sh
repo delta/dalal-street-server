9bd4ad917ebc4a5612d8731ab65552c314ac1a54
Parth Thakkar <thakkarparth007@gmail.com>
2017-01-30 20:47:08 +0530

    Add build_proto.sh to work with proto submodule

---
+++ socketapi/build_proto.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir -p proto_build/
+cp -R proto/* proto_build/
+cd proto_build/
+protoc --go_out=import_prefix=github.com/thakkarparth007/dalal-street-server/socketapi/proto_build/,import_path=socketapi:. *.proto
+protoc --go_out=import_prefix=github.com/thakkarparth007/dalal-street-server/socketapi/proto_build/,import_path=socketapi/actions:. actions/*.proto
+protoc --go_out=import_prefix=github.com/thakkarparth007/dalal-street-server/socketapi/proto_build/,import_path=socketapi/models:. models/*.proto
+protoc --go_out=import_prefix=github.com/thakkarparth007/dalal-street-server/socketapi/proto_build/,import_path=socketapi/errors:. errors/*.proto
+protoc --go_out=import_prefix=github.com/thakkarparth007/dalal-street-server/socketapi/proto_build/,import_path=socketapi/datastreams:. datastreams/*.proto
+grep -rl "github.com/golang/protobuf/proto" . | grep -v ".sh" | xargs sed -i 's|github.com/thakkarparth007/dalal-street-server/socketapi/proto_build/github.com/golang/protobuf/proto|github.com/golang/protobuf/proto|g'
+find -type f -name "*.proto" -exec rm {} \;
+go build
+
