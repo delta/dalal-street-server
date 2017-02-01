01d3c718c01fbb96e3f4f7e761c1e12e2db4ec1d
Parth Thakkar <thakkarparth007@gmail.com>
2017-02-01 07:31:47 +0530

    Fix build_proto.sh file to work on Mac

--- socketapi/build_proto.sh
+++ socketapi/build_proto.sh
@@ -8,7 +8,7 @@ protoc --go_out=import_prefix=github.com/thakkarparth007/dalal-street-server/soc
 protoc --go_out=import_prefix=github.com/thakkarparth007/dalal-street-server/socketapi/proto_build/,import_path=socketapi/models:. models/*.proto
 protoc --go_out=import_prefix=github.com/thakkarparth007/dalal-street-server/socketapi/proto_build/,import_path=socketapi/errors:. errors/*.proto
 protoc --go_out=import_prefix=github.com/thakkarparth007/dalal-street-server/socketapi/proto_build/,import_path=socketapi/datastreams:. datastreams/*.proto
-grep -rl "github.com/golang/protobuf/proto" . | grep -v ".sh" | xargs sed -i 's|github.com/thakkarparth007/dalal-street-server/socketapi/proto_build/github.com/golang/protobuf/proto|github.com/golang/protobuf/proto|g'
-find -type f -name "*.proto" -exec rm {} \;
+grep -rl "github.com/golang/protobuf/proto" . | grep -v ".sh" | xargs sed -i'' 's|github.com/thakkarparth007/dalal-street-server/socketapi/proto_build/github.com/golang/protobuf/proto|github.com/golang/protobuf/proto|g'
+find . -type f -name "*.proto" -exec rm {} \;
 go build
 
