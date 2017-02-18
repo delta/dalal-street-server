bc7280b162b848fbd13d969548cac99a9ff8f278
coderick14 <bhattacharyya.rick14@gmail.com>
2017-02-19 03:35:52 +0530

    Add BuyLimitExceededError to PerformBuyFromExchange

--- models/Constants.go
+++ models/Constants.go
@@ -3,6 +3,7 @@ package models
 const SHORT_SELL_BORROW_LIMIT = 20
 const BID_LIMIT = 20
 const ASK_LIMIT = 20
+const BUY_LIMIT = 20
 const MINIMUM_CASH_LIMIT = 0
 const BUY_FROM_EXCHANGE_LIMIT = 20
 
