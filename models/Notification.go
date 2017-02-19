1b4648b3a82bff5ed4cb5cb9a0eef461c3ae568e
coderick14 <bhattacharyya.rick14@gmail.com>
2017-02-19 22:01:25 +0530

    Add function GetNotifications

--- models/Notification.go
+++ models/Notification.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"github.com/Sirupsen/logrus"
 	models_proto "github.com/thakkarparth007/dalal-street-server/socketapi/proto_build/models"
 )
 
@@ -22,4 +23,45 @@ func (gNotification *Notification) ToProto() *models_proto.Notification {
 		Text:      gNotification.Text,
 		CreatedAt: gNotification.CreatedAt,
 	}
+}
+
+func GetNotifications(lastId, count uint32) (bool, map[uint32]*Notification, error) {
+	var l = logger.WithFields(logrus.Fields{
+		"method": "GetNotifications",
+		"lastId": lastId,
+		"count":  count,
+	})
+
+	l.Infof("Attempting to get notifications")
+
+	db, err := DbOpen()
+	if err != nil {
+		return true, nil, err
+	}
+	defer db.Close()
+
+	var notifications []*Notification
+
+	//set default value of count if it is zero
+	if count == 0 {
+		count = GET_NOTIFICATION_COUNT
+	}
+
+	//get latest events if lastId is zero
+	if lastId != 0 {
+		db = db.Where("id <= ?", lastId)
+	}
+	if err := db.Order("desc id").Limit(count).Find(&notifications).Error; err != nil {
+		return true, nil, err
+	}
+
+	notificationsMap := make(map[uint32]*Notification)
+
+	for _, notification := range notifications {
+		notificationsMap[notification.Id] = notification
+	}
+
+	var moreExists = len(notifications) < int(count)
+	l.Infof("Successfully fetched notifications")
+	return moreExists, notificationsMap, nil
 }
