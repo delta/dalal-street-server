b3f63449ff42877d726257270b1c7a47e756eba5
coderick14 <bhattacharyya.rick14@gmail.com>
2017-11-24 20:48:52 +0530

    Add files for dockerizing the application

--- docker-entry.sh
+++ docker-entry.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
+# Wait till mysql is accepting connections
 sleep 10
+
+# Run the migrations
+migrate -url "mysql://root:MYSQL_ROOT_PASSWORD@tcp(db:3306)/DalalStreet" -path ./migrations up
+
+# Start server
 go run main.go
