ef9ad63d1a1e7e5ecb261c9fd863c0754867a767
Parth Thakkar <thakkarparth007@gmail.com>
2017-12-26 04:13:55 +0530

    Sync proto. Update build_proto to generate gRPC code

--- build_proto.sh
+++ build_proto.sh
@@ -4,10 +4,11 @@ rm -rf proto_build
 mkdir -p proto_build/
 cp -R proto/* proto_build/
 cd proto_build/
-protoc --go_out=import_prefix=github.com/thakkarparth007/dalal-street-server/proto_build/,import_path=pb:. *.proto
+protoc --go_out=plugins=grpc,import_prefix=github.com/thakkarparth007/dalal-street-server/proto_build/,import_path=pb:. *.proto
 protoc --go_out=import_prefix=github.com/thakkarparth007/dalal-street-server/proto_build/,import_path=actions_pb:. actions/*.proto
 protoc --go_out=import_prefix=github.com/thakkarparth007/dalal-street-server/proto_build/,import_path=models_pb:. models/*.proto
 protoc --go_out=import_prefix=github.com/thakkarparth007/dalal-street-server/proto_build/,import_path=datastreams_pb:. datastreams/*.proto
-grep -rl "github.com/golang/protobuf/proto" . | grep -v ".sh" | xargs sed -i'' 's|github.com/thakkarparth007/dalal-street-server/proto_build/github.com/golang/protobuf/proto|github.com/golang/protobuf/proto|g'
+grep -rl "proto_build" . | grep -v ".sh" | xargs sed -r -i.bak 's|github.com/thakkarparth007/dalal-street-server/proto_build/(google\|golang\|github)|\1|g'
+find . -type f -name "*.bak" -exec rm {} \;
 find . -type f -name "*.proto" -exec rm {} \;
 go build
