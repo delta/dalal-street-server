12d1eba3edaad1d8abec1e5c7d0f112dce52a0b9
Ar-Sibi <getgoingwithsibi@gmail.com>
2017-12-27 01:08:27 +0530

    Added Test For UpdateStockPrice

--- models/Stock_test.go
+++ models/Stock_test.go
@@ -32,3 +32,44 @@ func TestStockToProto(t *testing.T) {
 		t.Fatal("Converted value not equal")
 	}
 }
+
+func Test_UpdateStockPrice(t *testing.T) {
+	stock := &Stock{
+		Id:               1,
+		CurrentPrice:     1000,
+		AllTimeLow:       1000,
+		PreviousDayClose: 1000,
+		AllTimeHigh:      1000,
+	}
+
+	db, err := DbOpen()
+	if err != nil {
+		t.Fatalf("Opening Database for inserting stocks failed  %v", err)
+	}
+	defer db.Close()
+
+	db.Save(stock)
+	defer func() {
+		db.Exec("DELETE FROM StockHistory")
+		db.Delete(stock)
+	}()
+
+	LoadStocks()
+	UpdateStockPrice(1, 2000)
+
+	a := &Stock{}
+	db.First(a, 1)
+	var stock1 = &Stock{
+		Id:               1,
+		CurrentPrice:     2000,
+		DayHigh:          2000,
+		AllTimeHigh:      2000,
+		AllTimeLow:       1000,
+		UpOrDown:         true,
+		AvgLastPrice:     850,
+		PreviousDayClose: 1000,
+	}
+	if !testutils.AssertEqual(t, stock1, a) {
+		t.Fatalf("Expected %v but got %v", stock1, a)
+	}
+}
