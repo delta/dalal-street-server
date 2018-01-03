71bb30b9cfb48d0cd554ebe175400e9a6c0000c6
Parth Thakkar <thakkarparth007@gmail.com>
2018-01-03 17:46:48 +0530

    Add TestMain for models

---
+++ models/models_main_test.go
@@ -0,0 +1,28 @@
+package models
+
+import (
+	"os"
+	"testing"
+
+	"github.com/Sirupsen/logrus"
+	"github.com/thakkarparth007/dalal-street-server/datastreams"
+	"github.com/thakkarparth007/dalal-street-server/utils"
+)
+
+func TestMain(m *testing.M) {
+	conf := utils.GetConfiguration()
+	utils.Init(conf)
+
+	// TODO: remove this and insert MarketEvent data stream as a dependency to actual code, then insert stub while using it
+	datastreams.Init(conf)
+
+	// Initialize the models package, but don't call the Init method. It does extra things we don't
+	// want to happen.
+	logger = utils.Logger.WithFields(logrus.Fields{
+		"module": "models",
+	})
+	// set the config global variable to the actual configuration
+	config = conf
+
+	os.Exit(m.Run())
+}
