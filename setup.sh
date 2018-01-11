73eadc8afdda1668e24136ba9f8b4b7ceb6d33df
paiakshay998 <paiakshay998@gmail.com>
2018-01-11 12:42:53 +0000

    Add script to deploy on server
    - Add gnupg as dependency in web container
    - Expose server on port 443
    - Add setup script to automate deployment
    - Run setup script from docker-entry

---
+++ setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+wget -N https://github.com/google/protobuf/releases/download/v3.2.0rc2/protoc-3.2.0rc2-linux-x86_64.zip
+
+echo "######## Unzipping protoc compiler ##########"
+unzip protoc-3.2.0rc2-linux-x86_64.zip -d protobuf
+
+echo "######## Adding to path ##########"
+export PATH=$PATH:$(pwd)/protobuf/bin
+
+echo "######## Fetching Go dependencies ##########"
+go get -u -v github.com/golang/protobuf/{proto,protoc-gen-go}
+go get -u -v golang.org/x/net/context
+go get -u -v google.golang.org/grpc
+go get -v github.com/gemnasium/migrate
+go get -v ./...
+
+echo "########## Building proto files ###########"
+bash build_proto.sh
+
+# Get the database password
+dbPass=$(egrep "Docker|DbPassword" config.json \
+        | grep -C1 "Docker" | tail -n1 \
+        | awk '{print substr($2,2,length($2)-3)}')
+
+echo "######### Running migrations ##########"
+migrate -url "mysql://root:$dbPass@tcp(db:3306)/dalalstreet_docker" -path ./migrations up
