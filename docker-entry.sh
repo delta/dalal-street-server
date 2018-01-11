fb8eeb00d6bb8af61db3c6581d0f34da4f43956b
paiakshay998 <paiakshay998@gmail.com>
2018-01-11 12:42:53 +0000

    Add script to deploy on server
    - Add gnupg as dependency in web container
    - Expose server on port 443
    - Add setup script to automate deployment
    - Run setup script from docker-entry

--- docker-entry.sh
+++ docker-entry.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
 # Wait till mysql is accepting connections
+echo "################## Waiting for mysql to accept incoming connections ##################"
 sleep 10
 
 # Run the migrations
-migrate -url "mysql://root:MYSQL_CONTAINER_ROOT_PASSWORD@tcp(db:3306)/dalalstreet_docker" -path ./migrations up
+echo "################## Running setup script ##################"
+bash setup.sh
 
 # Start server
 go run main.go
