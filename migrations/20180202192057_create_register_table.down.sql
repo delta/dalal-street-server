dc26dc4170eb273e990c9168a51e237c3be64f6e
Ar-Sibi <getgoingwithsibi@gmail.com>
2018-02-06 22:30:50 +0530

    Hash passwords,fix names

--- migrations/20180202192057_create_register_table.down.sql
+++ migrations/20180202192057_create_register_table.down.sql
@@ -1,1 +1,1 @@
-DROP TABLE IF EXISTS Registers;
+DROP TABLE IF EXISTS Registrations;
--- migrations/20180202192057_create_register_table.up.sql
+++ migrations/20180202192057_create_register_table.up.sql
@@ -1,7 +1,8 @@
-CREATE TABLE IF NOT EXISTS Registers(
+CREATE TABLE IF NOT EXISTS Registrations(
 	id int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
 	userId int(11) UNSIGNED NOT NULL,
 	email varchar(70) NOT NULL,
+	country varchar(70) NOT NULL,
 	userName varchar(100) NOT NULL,
 	fullName varchar(100) NOT NULL,
 	isPragyan BOOL NOT NULL,
