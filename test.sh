191496737c79b51241cca730e5a09724eb46fb8a
Gautham Kumar <heygautham@gmail.com>
2018-12-19 22:58:02 +0530

    [tests]: Clearer documention for the test script

--- test.sh
+++ test.sh
@@ -4,6 +4,9 @@
 export DALAL_ENV=Test
 
 # Unit tests
+# Any test that does NOT contain a "_" in its name is considered a unit test.
+# It is run without initializing any tables. If you required the database to be set up
+# for your test, write it as an Integration Test.
 go test -v -run="^(Test|Benchmark)[^_](.*)" ./... -args -config="$(pwd)/config.json"
 code=$?
 if [ $code -ne 0 ]; then
@@ -11,15 +14,21 @@ if [ $code -ne 0 ]; then
 fi
 
 # Integration tests
+printf "\nUnit Tests complete. Performing Integration Tests now.\n"
 
 # Get db password from "Test" section of config.json
 dbPass=$(egrep "Test|DbPassword" config.json \
 	| grep -C1 "Test" | tail -n1 \
 	| awk '{print substr($2,2,length($2)-3)}')
 
+# Set up database
 migrate -url mysql://root:$dbPass@/dalalstreet_test -path ./migrations up
+
+# Any test that DOES contain a "_" in its name is considered an integration test.
 go test -race -v -p=1 -run="^(Test|Benchmark)_(.*)" ./... -args -config="$(pwd)/config.json"
 code=$?
+
+# Tear down database
 migrate -url mysql://root:$dbPass@/dalalstreet_test -path ./migrations down 
 
 exit $code
