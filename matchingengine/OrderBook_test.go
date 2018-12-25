71dd7a97eecd725c5b4be9c8a92350a1018a0144
Gautham Kumar <heygautham@gmail.com>
2018-12-25 11:28:08 +0530

    [matchingengine]: 2nd batch of OrderBook tests added

--- matchingengine/OrderBook_test.go
+++ matchingengine/OrderBook_test.go
@@ -10,6 +10,20 @@ import (
 	"github.com/golang/mock/gomock"
 )
 
+// helper function to create a new transaction object
+func makeTransaction(id uint32, userID uint32, stockID uint32, transactionType models.TransactionType, stockQty int32, price uint32, total int32, createdAt string) *models.Transaction {
+	return &models.Transaction{
+		Id:            id,
+		UserId:        userID,
+		StockId:       stockID,
+		Type:          transactionType,
+		StockQuantity: stockQty,
+		Price:         price,
+		Total:         total,
+		CreatedAt:     createdAt,
+	}
+}
+
 func TestLoadOldAsk(t *testing.T) {
 
 	// This initializes the logger. This is required because LoadOldAsk tries accessing the logger
@@ -52,4 +66,179 @@ func TestLoadOldAsk(t *testing.T) {
 
 	ob.LoadOldAsk(limitAsk)
 
+}
+
+func TestLoadOldAskStopLoss(t *testing.T) {
+
+	// This initializes the logger. This is required because LoadOldAsk tries accessing the logger
+	// and if it is not initialized, a nil reference error is thrown.
+	config := utils.GetConfiguration()
+	utils.Init(config)
+
+	mockControl := gomock.NewController(t)
+	defer mockControl.Finish()
+
+	var stockID uint32 = 1
+	var stockQuantity uint32 = 10
+	var stockPrice uint32 = 20
+
+	mockAskQueue := mocks.NewMockAskPQueue(mockControl)
+	mockBidQueue := mocks.NewMockBidPQueue(mockControl)
+	mockAskStoplossQueue := mocks.NewMockAskPQueue(mockControl)
+	mockBidStoplossQueue := mocks.NewMockBidPQueue(mockControl)
+	mockDepth := mocks.NewMockMarketDepthStream(mockControl)
+
+	ob := &orderBook{
+		logger: utils.Logger.WithFields(logrus.Fields{
+			"module":        "matchingengine.OrderBook.test",
+			"param_stockId": stockID,
+		}),
+		stockId:     stockID,
+		askChan:     make(chan *models.Ask),
+		bidChan:     make(chan *models.Bid),
+		asks:        mockAskQueue,
+		bids:        mockBidQueue,
+		askStoploss: mockAskStoplossQueue,
+		bidStoploss: mockBidStoplossQueue,
+		depth:       mockDepth,
+	}
+
+	stopLossAsk := makeAsk(1, stockID, models.StopLoss, stockQuantity, stockPrice, "")
+
+	mockAskStoplossQueue.EXPECT().Push(stopLossAsk).Times(1)
+
+	ob.LoadOldAsk(stopLossAsk)
+
+}
+
+func TestLoadOldBid(t *testing.T) {
+
+	// This initializes the logger. This is required because LoadOldBid tries accessing the logger
+	// and if it is not initialized, a nil reference error is thrown.
+	config := utils.GetConfiguration()
+	utils.Init(config)
+
+	mockControl := gomock.NewController(t)
+	defer mockControl.Finish()
+
+	var stockID uint32 = 1
+	var stockQuantity uint32 = 10
+	var stockPrice uint32 = 20
+
+	mockAskQueue := mocks.NewMockAskPQueue(mockControl)
+	mockBidQueue := mocks.NewMockBidPQueue(mockControl)
+	mockAskStoplossQueue := mocks.NewMockAskPQueue(mockControl)
+	mockBidStoplossQueue := mocks.NewMockBidPQueue(mockControl)
+	mockDepth := mocks.NewMockMarketDepthStream(mockControl)
+
+	ob := &orderBook{
+		logger: utils.Logger.WithFields(logrus.Fields{
+			"module":        "matchingengine.OrderBook.test",
+			"param_stockId": stockID,
+		}),
+		stockId:     stockID,
+		askChan:     make(chan *models.Ask),
+		bidChan:     make(chan *models.Bid),
+		asks:        mockAskQueue,
+		bids:        mockBidQueue,
+		askStoploss: mockAskStoplossQueue,
+		bidStoploss: mockBidStoplossQueue,
+		depth:       mockDepth,
+	}
+
+	limitBid := makeBid(1, stockID, models.Limit, stockQuantity, stockPrice, "")
+
+	mockBidQueue.EXPECT().Push(limitBid).Times(1)
+	mockDepth.EXPECT().AddOrder(false, false, stockPrice, stockQuantity).Times(1)
+
+	ob.LoadOldBid(limitBid)
+
+}
+
+func TestLoadOldBidStopLoss(t *testing.T) {
+
+	// This initializes the logger. This is required because LoadOldAsk tries accessing the logger
+	// and if it is not initialized, a nil reference error is thrown.
+	config := utils.GetConfiguration()
+	utils.Init(config)
+
+	mockControl := gomock.NewController(t)
+	defer mockControl.Finish()
+
+	var stockID uint32 = 1
+	var stockQuantity uint32 = 10
+	var stockPrice uint32 = 20
+
+	mockAskQueue := mocks.NewMockAskPQueue(mockControl)
+	mockBidQueue := mocks.NewMockBidPQueue(mockControl)
+	mockAskStoplossQueue := mocks.NewMockAskPQueue(mockControl)
+	mockBidStoplossQueue := mocks.NewMockBidPQueue(mockControl)
+	mockDepth := mocks.NewMockMarketDepthStream(mockControl)
+
+	ob := &orderBook{
+		logger: utils.Logger.WithFields(logrus.Fields{
+			"module":        "matchingengine.OrderBook.test",
+			"param_stockId": stockID,
+		}),
+		stockId:     stockID,
+		askChan:     make(chan *models.Ask),
+		bidChan:     make(chan *models.Bid),
+		asks:        mockAskQueue,
+		bids:        mockBidQueue,
+		askStoploss: mockAskStoplossQueue,
+		bidStoploss: mockBidStoplossQueue,
+		depth:       mockDepth,
+	}
+
+	stopLossBid := makeBid(1, stockID, models.StopLoss, stockQuantity, stockPrice, "")
+
+	mockBidStoplossQueue.EXPECT().Push(stopLossBid).Times(1)
+
+	ob.LoadOldBid(stopLossBid)
+
+}
+
+func TestLoadOldTransactions(t *testing.T) {
+
+	// This initializes the logger. This is required because LoadOldAsk tries accessing the logger
+	// and if it is not initialized, a nil reference error is thrown.
+	config := utils.GetConfiguration()
+	utils.Init(config)
+
+	mockControl := gomock.NewController(t)
+	defer mockControl.Finish()
+
+	var stockID uint32 = 1
+
+	mockAskQueue := mocks.NewMockAskPQueue(mockControl)
+	mockBidQueue := mocks.NewMockBidPQueue(mockControl)
+	mockAskStoplossQueue := mocks.NewMockAskPQueue(mockControl)
+	mockBidStoplossQueue := mocks.NewMockBidPQueue(mockControl)
+	mockDepth := mocks.NewMockMarketDepthStream(mockControl)
+
+	ob := &orderBook{
+		logger: utils.Logger.WithFields(logrus.Fields{
+			"module":        "matchingengine.OrderBook.test",
+			"param_stockId": stockID,
+		}),
+		stockId:     stockID,
+		askChan:     make(chan *models.Ask),
+		bidChan:     make(chan *models.Bid),
+		asks:        mockAskQueue,
+		bids:        mockBidQueue,
+		askStoploss: mockAskStoplossQueue,
+		bidStoploss: mockBidStoplossQueue,
+		depth:       mockDepth,
+	}
+
+	t1 := makeTransaction(1, 10, stockID, 1, 20, 50, 1000, "random1")
+	t2 := makeTransaction(2, 10, stockID, 2, 10, 20, 200, "random2")
+
+	transactions := []*models.Transaction{t1, t2}
+
+	for _, transaction := range transactions {
+		mockDepth.EXPECT().AddTrade(transaction.Price, uint32(-transaction.StockQuantity), transaction.CreatedAt).Times(1)
+	}
+
+	ob.LoadOldTransactions(transactions)
 }
