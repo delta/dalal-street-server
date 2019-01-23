5227b8404f88c141915419c922ef113d21e005bd
paiakshay998 <paiakshay998@gmail.com>
2019-01-23 18:10:37 +0530

    [orders]: Create OrderDepositTransactions and reserve transaction types

---
+++ migrations/20190122135305_create_order_deposit_transactions.up.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS OrderDepositTransactions (
+	orderId int(11) UNSIGNED NOT NULL,
+    transactionId int(11) UNSIGNED NOT NULL,
+    isAsk tinyint(1) NOT NULL,
+    createdAt varchar(255) NOT NULL DEFAULT "0000-00-00T00:00:00+05:30",
+	PRIMARY KEY (orderId, transactionId, isAsk)
+);
