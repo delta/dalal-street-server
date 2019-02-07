513cd5443e61ba462db723ff7b018f4b629de027
Gautham Kumar <heygautham@gmail.com>
2019-02-07 10:26:10 +0530

    [proto]: Fix golang context not found error

--- build_proto.sh
+++ build_proto.sh
@@ -8,7 +8,7 @@ protoc --go_out=plugins=grpc,import_prefix=github.com/delta/dalal-street-server/
 protoc --go_out=import_prefix=github.com/delta/dalal-street-server/proto_build/,import_path=actions_pb:. actions/*.proto
 protoc --go_out=import_prefix=github.com/delta/dalal-street-server/proto_build/,import_path=models_pb:. models/*.proto
 protoc --go_out=import_prefix=github.com/delta/dalal-street-server/proto_build/,import_path=datastreams_pb:. datastreams/*.proto
-grep -rl "proto_build" . | grep -v ".sh" | xargs sed -E -i.bak 's|github.com/delta/dalal-street-server/proto_build/(google\|golang\|github)|\1|g'
+grep -rl "proto_build" . | grep -v ".sh" | xargs sed -E -i.bak 's|github.com/delta/dalal-street-server/proto_build/(google\|golang\|github\|context)|\1|g'
 find . -type f -name "*.bak" -exec rm {} \;
 find . -type f -name "*.proto" -exec rm {} \;
 go build
