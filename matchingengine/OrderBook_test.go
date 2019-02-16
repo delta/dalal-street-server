6583ca150a90dbeebe1246cab98952fb72dbb648
Adwaith <adwaith3208@gmail.com>
2019-02-16 09:55:59 +0530

    4th batch of tests for matching engine

--- matchingengine/OrderBook_test.go
+++ matchingengine/OrderBook_test.go
@@ -1,7 +1,6 @@
 package matchingengine
 
 import (
-	"fmt"
 	"testing"
 	"time"
 
@@ -25,13 +24,8 @@ func helperForOrderBookTests(t *testing.T) (
 	uint32,
 	uint64,
 	uint64) {
-	// This initializes the logger. This is required because LoadOldAsk tries accessing the logger
-	// and if it is not initialized, a nil reference error is thrown.
-	// config := utils.GetConfiguration()
-	// utils.Init(config)
 
 	mockControl := gomock.NewController(t)
-	// defer mockControl.Finish()
 
 	var stockID uint32 = 1
 	var stockQuantity uint64 = 10
@@ -72,13 +66,8 @@ func helperForOrderBookTestsWithoutMockPQ(t *testing.T) (
 	uint32,
 	uint64,
 	uint64) {
-	// This initializes the logger. This is required because LoadOldAsk tries accessing the logger
-	// and if it is not initialized, a nil reference error is thrown.
-	// config := utils.GetConfiguration()
-	// utils.Init(config)
 
 	mockControl := gomock.NewController(t)
-	// defer mockControl.Finish()
 
 	var stockID uint32 = 1
 	var stockQuantity uint64 = 10
@@ -257,17 +246,17 @@ func TestOrderBookAddAskOrder(t *testing.T) {
 	limitAsk2 := makeAsk(1, stockID+1, models.Limit, stockQuantity+10, stockPrice+20, "")
 
 	go ob.AddAskOrder(limitAsk1)
-	time.Sleep(time.Second)
+	time.Sleep(time.Second * 1)
 	go ob.AddAskOrder(limitAsk2)
 
 	recAsk1, ok := <-ob.askChan
 	if !ok || recAsk1 != limitAsk1 {
-		t.Fail()
+		t.Errorf("1 %v %v - %v", recAsk1, limitAsk1, ok)
 	}
 
 	recAsk2, ok := <-ob.askChan
 	if !ok || recAsk2 != limitAsk2 {
-		t.Fail()
+		t.Errorf("2 %v %v - %v", recAsk2, limitAsk2, ok)
 	}
 
 }
@@ -299,7 +288,6 @@ func TestOrderBookAddBidOrder(t *testing.T) {
 
 	go ob.AddBidOrder(limitBid1)
 	time.Sleep(time.Second)
-
 	go ob.AddBidOrder(limitBid2)
 
 	recBid1, ok := <-ob.bidChan
@@ -389,41 +377,6 @@ func TestOrderBookCancelBidOrderStoploss(t *testing.T) {
 
 }
 
-//TestStartStockMatching has tests for both StartStockMatching and clearExistingOrders
-func TestStartStockMatching(t *testing.T) {
-
-	config := utils.GetConfiguration()
-	utils.Init(config)
-
-	mockControl, ob, mockAskQueue, mockBidQueue, _, _, mockMarketDepth, stockID, stockQuantity, stockPrice := helperForOrderBookTests(t)
-	defer mockControl.Finish()
-
-	limitBid := makeBid(1, stockID, models.Limit, stockQuantity, stockPrice, "")
-	limitAsk := makeAsk(3, stockID, models.Limit, stockQuantity, stockPrice, "")
-
-	mockBidQueue.EXPECT().Head().AnyTimes()
-	mockBidQueue.EXPECT().Size().AnyTimes()
-	mockBidQueue.EXPECT().Push(limitBid)
-
-	mockAskQueue.EXPECT().Head().AnyTimes()
-	mockAskQueue.EXPECT().Size().AnyTimes()
-	mockAskQueue.EXPECT().Push(limitAsk)
-
-	mockMarketDepth.EXPECT().AddOrder(false, true, stockPrice, stockQuantity).Times(1)
-	mockMarketDepth.EXPECT().AddOrder(false, false, stockPrice, stockQuantity).Times(1)
-
-	ob.StartStockMatching()
-	go ob.AddAskOrder(limitAsk)
-	go ob.AddBidOrder(limitBid)
-
-	time.Sleep(time.Second * 5)
-
-	if mockAskQueue.Head() != nil || mockBidQueue.Head() != nil {
-		fmt.Println(mockAskQueue.Head())
-	}
-
-}
-
 func TestTopMatchingAsk(t *testing.T) {
 
 	config := utils.GetConfiguration()
@@ -437,9 +390,7 @@ func TestTopMatchingAsk(t *testing.T) {
 
 	mockMarketDepth.EXPECT().AddOrder(true, true, stockPrice, stockQuantity).Times(1)
 	go ob.StartStockMatching()
-	go ob.AddAskOrder(marketAsk)
-
-	time.Sleep(time.Second * 2)
+	ob.AddAskOrder(marketAsk)
 
 	topAsk, _ := ob.getTopMatchingAsk(marketBid)
 
@@ -463,9 +414,7 @@ func TestTopMatchingBid(t *testing.T) {
 	mockMarketDepth.EXPECT().AddOrder(true, false, stockPrice, stockQuantity).Times(1)
 
 	go ob.StartStockMatching()
-	go ob.AddBidOrder(marketBid)
-
-	time.Sleep(time.Second * 2)
+	ob.AddBidOrder(marketBid)
 
 	topBid, _ := ob.getTopMatchingBid(marketAsk)
 
@@ -475,170 +424,15 @@ func TestTopMatchingBid(t *testing.T) {
 
 }
 
-func TestProcessAsk(t *testing.T) {
-
-	config := utils.GetConfiguration()
-	utils.Init(config)
-
-	mockControl, ob, mockAskQueue, mockBidQueue, _, _, mockMarketDepth, stockID, stockQuantity, stockPrice := helperForOrderBookTests(t)
-	defer mockControl.Finish()
-
-	marketBid := makeBid(1, stockID, models.Market, stockQuantity, stockPrice, "")
-	marketAsk := makeAsk(3, stockID, models.Market, stockQuantity, stockPrice, "")
-
-	mockBidQueue.EXPECT().Head().AnyTimes()
-	mockBidQueue.EXPECT().Size().AnyTimes()
-
-	mockAskQueue.EXPECT().Head().AnyTimes()
-	mockAskQueue.EXPECT().Size().AnyTimes()
-	mockAskQueue.EXPECT().Push(marketAsk)
-
-	mockMarketDepth.EXPECT().AddOrder(true, true, stockPrice, stockQuantity).Times(1)
-
-	go ob.AddAskOrder(marketAsk)
-	go ob.AddBidOrder(marketBid)
-	time.Sleep(time.Second)
-
-	go ob.processAsk(marketAsk)
-	time.Sleep(time.Second * 2)
-
-	if mockAskQueue.Head() != nil || mockBidQueue.Head() != nil {
-		t.Errorf("Ask was not processed")
-	}
-
-}
-
-func TestProcessBid(t *testing.T) {
-
-	config := utils.GetConfiguration()
-	utils.Init(config)
-
-	mockControl, ob, mockAskQueue, mockBidQueue, _, _, mockMarketDepth, stockID, stockQuantity, stockPrice := helperForOrderBookTests(t)
-	defer mockControl.Finish()
-
-	marketBid := makeBid(1, stockID, models.Market, stockQuantity, stockPrice, "")
-	marketAsk := makeAsk(3, stockID, models.Market, stockQuantity, stockPrice, "")
-
-	mockBidQueue.EXPECT().Head().AnyTimes()
-	mockBidQueue.EXPECT().Size().AnyTimes()
-	mockBidQueue.EXPECT().Push(marketBid)
-
-	mockAskQueue.EXPECT().Head().AnyTimes()
-	mockAskQueue.EXPECT().Size().AnyTimes()
-
-	mockMarketDepth.EXPECT().AddOrder(true, false, stockPrice, stockQuantity).Times(1)
-
-	go ob.AddAskOrder(marketAsk)
-	time.Sleep(time.Second * 1)
-	go ob.AddBidOrder(marketBid)
-	time.Sleep(time.Second * 2)
-
-	ob.processBid(marketBid)
-
-	if mockBidQueue.Head() != nil || mockAskQueue.Head() != nil {
-		t.Errorf("Bid was not processed")
-	}
-
-}
-
-func TestTriggerStoplosses(t *testing.T) {
-	config := utils.GetConfiguration()
-	utils.Init(config)
-
-	mockControl, ob, _, _, mockAskStoplossQueue, mockBidStoplossQueue, _, stockID, stockQuantity, _ := helperForOrderBookTests(t)
-	defer mockControl.Finish()
-
-	t1 := &models.Transaction{
-		Id:            1,
-		UserId:        10,
-		StockId:       stockID,
-		Type:          1,
-		StockQuantity: 20,
-		Price:         50,
-		Total:         1000,
-		CreatedAt:     "random1",
-	}
-
-	stoplossBid := makeBid(1, stockID, models.StopLoss, stockQuantity, 30, "")
-	stoplossAsk := makeAsk(3, stockID, models.StopLoss, stockQuantity, 70, "")
-
-	mockBidStoplossQueue.EXPECT().Head().AnyTimes()
-	mockBidStoplossQueue.EXPECT().Size().AnyTimes()
-	mockBidStoplossQueue.EXPECT().Push(stoplossBid)
-
-	mockAskStoplossQueue.EXPECT().Head().AnyTimes()
-	mockAskStoplossQueue.EXPECT().Size().AnyTimes()
-	mockAskStoplossQueue.EXPECT().Push(stoplossAsk)
-
-	go ob.AddAskOrder(stoplossAsk)
-	go ob.AddBidOrder(stoplossBid)
-	time.Sleep(time.Second)
-	go ob.triggerStopLosses(t1)
-	time.Sleep(time.Second * 2)
-
-	if ob.askStoploss.Head() != nil || ob.bidStoploss.Head() != nil {
-		t.Errorf("Stoploss was not triggered")
-	}
-}
-
-func TestWaitForOrders(t *testing.T) {
-	config := utils.GetConfiguration()
-	utils.Init(config)
-
-	mockControl, ob, mockAskQueue, mockBidQueue, _, _, mockMarketDepth, stockID, stockQuantity, stockPrice := helperForOrderBookTests(t)
-	defer mockControl.Finish()
-
-	marketBid := makeBid(1, stockID, models.Market, stockQuantity, stockPrice, "")
-	marketAsk := makeAsk(3, stockID, models.Market, stockQuantity, stockPrice, "")
-
-	mockBidQueue.EXPECT().Head().AnyTimes()
-	mockBidQueue.EXPECT().Size().AnyTimes()
-
-	mockAskQueue.EXPECT().Head().AnyTimes()
-	mockAskQueue.EXPECT().Size().AnyTimes()
-	mockAskQueue.EXPECT().Push(marketAsk)
-
-	mockMarketDepth.EXPECT().AddOrder(true, true, stockPrice, stockQuantity).Times(1)
-
-	go ob.waitForOrder()
-	time.Sleep(time.Second)
-	go ob.AddAskOrder(marketAsk)
-	time.Sleep(time.Second)
-	go ob.AddBidOrder(marketBid)
-	time.Sleep(time.Second)
-
-	if ob.asks.Head() != nil || ob.bids.Head() != nil {
-		t.Errorf("Wait for order failed")
-	}
-}
-
-// func Test_MakeTrade(t *testing.T) { // to be completed
-// 	ob, _, _, _, _, mockDepth, stockID, stockQuantity, stockPrice := helperForOrderBookTests(t)
-
-// 	marketBid := makeBid(1, stockID, models.Market, stockQuantity, stockPrice, "")
-// 	marketAsk := makeAsk(3, stockID, models.Market, stockQuantity, stockPrice, "")
-
-// 	go ob.AddAskOrder(marketAsk)
-// 	go ob.AddBidOrder(marketBid)
-// 	time.Sleep(time.Second * 2)
-
-// 	mockDepth.EXPECT().CloseOrder(isMarket(marketBid.OrderType), false, marketBid.Price, uint32(-stockQuantity))
-// 	mockDepth.EXPECT().CloseOrder(isMarket(marketAsk.OrderType), false, marketAsk.Price, uint32(-stockQuantity))
-// 	mockDepth.EXPECT().AddTrade(gomock.Any, gomock.Any, gomock.Any)
-
-// askStatus, bidStatus := ob.makeTrade(marketAsk, marketBid, false, false)
-
-// 	if !askStatus || !bidStatus {
-// 		t.Errorf("Trade not done")
-// 	}
-// }
-
 func Test_MakeTrade(t *testing.T) {
 
 	config := utils.GetConfiguration()
 	utils.Init(config)
+	models.Init(config, datastreams.GetManager())
 
-	var l = utils.Logger.WithFields(logrus.Fields{})
+	var l = utils.Logger.WithFields(logrus.Fields{
+		"method": "Test_MakeTrade",
+	})
 
 	var stockID uint32 = 1
 	// var stockQuantity uint32 = 10
@@ -675,6 +469,9 @@ func Test_MakeTrade(t *testing.T) {
 	db := utils.GetDB()
 
 	defer func() {
+		db.Exec("DELETE from OrderFills")
+		db.Exec("DELETE from Transactions")
+		db.Exec("DELETE from TransactionSummary")
 		db.Delete(ask)
 		db.Delete(bid)
 		db.Delete(user1)
@@ -725,12 +522,355 @@ func Test_MakeTrade(t *testing.T) {
 
 	ob.LoadOldAsk(ask)
 	ob.LoadOldBid(bid)
+	models.LoadStocks()
 	time.Sleep(time.Second * 2)
 
 	askStatus, bidStatus := ob.makeTrade(ask, bid, false, false)
 
 	if !askStatus || !bidStatus {
 		l.Errorf("Errored in testMakeTrade")
+	}
+
+}
+
+func Test_ProcessAsk(t *testing.T) {
+
+	config := utils.GetConfiguration()
+	utils.Init(config)
+	models.Init(config, datastreams.GetManager())
+
+	var l = utils.Logger.WithFields(logrus.Fields{
+		"method": "Test_ProcessAsk",
+	})
+
+	var stockID uint32 = 1
+	// var stockQuantity uint32 = 10
+	// var stockPrice uint32 = 20
+	var userID1 uint32 = 3
+	var userID2 uint32 = 4
+
+	var makeAsk = func(userId uint32, stockId uint32, isClosed bool) *models.Ask {
+		return &models.Ask{
+			UserId:   userId,
+			StockId:  stockId,
+			IsClosed: isClosed,
+		}
+	}
+
+	var makeBid = func(userId uint32, stockId uint32, isClosed bool) *models.Bid {
+		return &models.Bid{
+			UserId:   userId,
+			StockId:  stockId,
+			IsClosed: isClosed,
+		}
+	}
+
+	stock := &models.Stock{
+		Id: 1,
+	}
+
+	user1 := &models.User{Id: userID1, Cash: 2000000, Email: "test1@test.com"}
+	user2 := &models.User{Id: userID2, Cash: 2000000, Email: "test2@test.com"}
+
+	ask := makeAsk(3, 1, false)
+	bid := makeBid(4, 1, false)
+
+	db := utils.GetDB()
+
+	defer func() {
+		db.Exec("DELETE from OrderFills")
+		db.Exec("DELETE from Transactions")
+		db.Exec("DELETE from TransactionSummary")
+		db.Delete(ask)
+		db.Delete(bid)
+		db.Delete(user1)
+		db.Delete(user2)
+		db.Delete(stock)
+	}()
+
+	if err := db.Create(user1).Error; err != nil {
+		t.Fatal(err)
+	}
+
+	if err := db.Create(user2).Error; err != nil {
+		t.Fatal(err)
+	}
+
+	if err := db.Create(stock).Error; err != nil {
+		t.Fatal(err)
+	}
+
+	if err := db.Create(ask).Error; err != nil {
+		t.Fatal(err)
+	}
+
+	if err := db.Create(bid).Error; err != nil {
+		t.Fatal(err)
+	}
+
+	testAskQueue := NewAskPQueue(1)
+	testBidQueue := NewBidPQueue(0)
+	testAskStoplossQueue := NewAskPQueue(1)
+	testBidStoplossQueue := NewBidPQueue(0)
+	testDepth := datastreams.GetManager().GetMarketDepthStream(stockID)
+
+	ob := &orderBook{
+		logger: utils.Logger.WithFields(logrus.Fields{
+			"module":        "matchingengine.OrderBook.test",
+			"param_stockId": stockID,
+		}),
+		stockId:     stockID,
+		askChan:     make(chan *models.Ask),
+		bidChan:     make(chan *models.Bid),
+		asks:        testAskQueue,
+		bids:        testBidQueue,
+		askStoploss: testAskStoplossQueue,
+		bidStoploss: testBidStoplossQueue,
+		depth:       testDepth,
+	}
+
+	ob.LoadOldAsk(ask)
+	ob.LoadOldBid(bid)
+	models.LoadStocks()
+	time.Sleep(time.Second * 2)
+
+	ob.processAsk(ask)
+
+	if ob.asks.Head() != nil || ob.bids.Head() != nil {
+		l.Errorf("Errored in processAsk")
+	}
+
+}
+
+func Test_ProcessBid(t *testing.T) {
+
+	config := utils.GetConfiguration()
+	utils.Init(config)
+	models.Init(config, datastreams.GetManager())
+
+	var l = utils.Logger.WithFields(logrus.Fields{
+		"method": "Test_ProcessBid",
+	})
+
+	var stockID uint32 = 1
+	// var stockQuantity uint32 = 10
+	// var stockPrice uint32 = 20
+	var userID1 uint32 = 3
+	var userID2 uint32 = 4
+
+	var makeAsk = func(userId uint32, stockId uint32, isClosed bool) *models.Ask {
+		return &models.Ask{
+			UserId:   userId,
+			StockId:  stockId,
+			IsClosed: isClosed,
+		}
+	}
+
+	var makeBid = func(userId uint32, stockId uint32, isClosed bool) *models.Bid {
+		return &models.Bid{
+			UserId:   userId,
+			StockId:  stockId,
+			IsClosed: isClosed,
+		}
+	}
+
+	stock := &models.Stock{
+		Id: 1,
+	}
+
+	user1 := &models.User{Id: userID1, Cash: 2000000, Email: "test1@test.com"}
+	user2 := &models.User{Id: userID2, Cash: 2000000, Email: "test2@test.com"}
+
+	ask := makeAsk(3, 1, false)
+	bid := makeBid(4, 1, false)
+
+	db := utils.GetDB()
+
+	defer func() {
+		db.Exec("DELETE from OrderFills")
+		db.Exec("DELETE from Transactions")
+		db.Exec("DELETE from TransactionSummary")
+		db.Delete(ask)
+		db.Delete(bid)
+		db.Delete(user1)
+		db.Delete(user2)
+		db.Delete(stock)
+	}()
+
+	if err := db.Create(user1).Error; err != nil {
+		t.Fatal(err)
+	}
+
+	if err := db.Create(user2).Error; err != nil {
+		t.Fatal(err)
+	}
+
+	if err := db.Create(stock).Error; err != nil {
+		t.Fatal(err)
+	}
+
+	if err := db.Create(ask).Error; err != nil {
+		t.Fatal(err)
+	}
+
+	if err := db.Create(bid).Error; err != nil {
+		t.Fatal(err)
+	}
+
+	testAskQueue := NewAskPQueue(1)
+	testBidQueue := NewBidPQueue(0)
+	testAskStoplossQueue := NewAskPQueue(1)
+	testBidStoplossQueue := NewBidPQueue(0)
+	testDepth := datastreams.GetManager().GetMarketDepthStream(stockID)
+
+	ob := &orderBook{
+		logger: utils.Logger.WithFields(logrus.Fields{
+			"module":        "matchingengine.OrderBook.test",
+			"param_stockId": stockID,
+		}),
+		stockId:     stockID,
+		askChan:     make(chan *models.Ask),
+		bidChan:     make(chan *models.Bid),
+		asks:        testAskQueue,
+		bids:        testBidQueue,
+		askStoploss: testAskStoplossQueue,
+		bidStoploss: testBidStoplossQueue,
+		depth:       testDepth,
+	}
+
+	ob.LoadOldAsk(ask)
+	ob.LoadOldBid(bid)
+	models.LoadStocks()
+	time.Sleep(time.Second * 2)
+
+	ob.processBid(bid)
+
+	if ob.asks.Head() != nil || ob.bids.Head() != nil {
+		l.Errorf("Errored in processBid")
+	}
+
+}
+
+func Test_TriggerStoploss(t *testing.T) {
+
+	config := utils.GetConfiguration()
+	utils.Init(config)
+	models.Init(config, datastreams.GetManager())
+
+	var l = utils.Logger.WithFields(logrus.Fields{
+		"method": "Test_ProcessBid",
+	})
+
+	var stockID uint32 = 1
+	var stockQuantity uint64 = 10
+	// var stockPrice uint32 = 20
+	var userID1 uint32 = 3
+	var userID2 uint32 = 4
+
+	var makeAsk = func(userId uint32, stockId uint32, ot models.OrderType, stockQty uint64, price uint64, placedAt string) *models.Ask {
+		return &models.Ask{
+			UserId:        userId,
+			StockId:       stockId,
+			OrderType:     ot,
+			StockQuantity: stockQty,
+			Price:         price,
+			CreatedAt:     placedAt,
+		}
+	}
+
+	var makeBid = func(userId uint32, stockId uint32, ot models.OrderType, stockQty uint64, price uint64, placedAt string) *models.Bid {
+		return &models.Bid{
+			UserId:        userId,
+			StockId:       stockId,
+			OrderType:     ot,
+			StockQuantity: stockQty,
+			Price:         price,
+			CreatedAt:     placedAt,
+		}
+	}
+
+	stock := &models.Stock{
+		Id:               1,
+		StocksInExchange: 100,
+	}
+
+	user1 := &models.User{Id: userID1, Cash: 2000000, Email: "test1@test.com"}
+	user2 := &models.User{Id: userID2, Cash: 2000000, Email: "test2@test.com"}
+
+	stoplossBid := makeBid(userID1, stockID, models.StopLoss, stockQuantity, 30, "")
+	stoplossAsk := makeAsk(userID2, stockID, models.StopLoss, stockQuantity, 70, "")
+
+	db := utils.GetDB()
+
+	defer func() {
+		db.Exec("DELETE from OrderFills")
+		db.Exec("DELETE from Transactions")
+		db.Exec("DELETE from TransactionSummary")
+		db.Delete(stoplossAsk)
+		db.Delete(stoplossBid)
+		db.Delete(user1)
+		db.Delete(user2)
+		db.Delete(stock)
+	}()
+
+	if err := db.Create(user1).Error; err != nil {
+		t.Fatal(err)
+	}
+
+	if err := db.Create(user2).Error; err != nil {
+		t.Fatal(err)
+	}
+
+	if err := db.Create(stock).Error; err != nil {
+		t.Fatal(err)
+	}
+
+	if err := db.Create(stoplossAsk).Error; err != nil {
+		t.Fatal(err)
+	}
+
+	if err := db.Create(stoplossBid).Error; err != nil {
+		t.Fatal(err)
+	}
+
+	testAskQueue := NewAskPQueue(1)
+	testBidQueue := NewBidPQueue(0)
+	testAskStoplossQueue := NewAskPQueue(1)
+	testBidStoplossQueue := NewBidPQueue(0)
+	testDepth := datastreams.GetManager().GetMarketDepthStream(stockID)
+
+	ob := &orderBook{
+		logger: utils.Logger.WithFields(logrus.Fields{
+			"module":        "matchingengine.OrderBook.test",
+			"param_stockId": stockID,
+		}),
+		stockId:     stockID,
+		askChan:     make(chan *models.Ask),
+		bidChan:     make(chan *models.Bid),
+		asks:        testAskQueue,
+		bids:        testBidQueue,
+		askStoploss: testAskStoplossQueue,
+		bidStoploss: testBidStoplossQueue,
+		depth:       testDepth,
+	}
+
+	t1 := &models.Transaction{
+		Id:            1,
+		UserId:        10,
+		StockId:       stockID,
+		Type:          1,
+		StockQuantity: 20,
+		Price:         50,
+		Total:         1000,
+		CreatedAt:     "random1",
+	}
+
+	ob.LoadOldAsk(stoplossAsk)
+	ob.LoadOldBid(stoplossBid)
+	ob.triggerStopLosses(t1)
+
+	if ob.askStoploss.Head() != nil || ob.bidStoploss.Head() != nil {
+		l.Errorf("Errored in processBid")
 	}
 
 }
