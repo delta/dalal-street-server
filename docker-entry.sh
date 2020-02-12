5cc49528d6d3c2a1eccf9fe763d0338a0244859f
nimishagarwal76 <nimishagarwal76@gmail.com>
2020-02-12 17:48:04 +0530

    Fix Docker

--- docker-entry.sh
+++ docker-entry.sh
@@ -4,6 +4,21 @@ set -x
 
 #tail -f /dev/null
 
+
+wget --tries=3 https://github.com/google/protobuf/releases/download/v3.2.0rc2/protoc-3.2.0rc2-linux-x86_64.zip -O protoc-3.2.0rc2-linux-x86_64.zip
+
+echo "######## Unzipping protoc compiler ##########"
+unzip protoc-3.2.0rc2-linux-x86_64.zip -d /root/protobuf
+
+echo "######## Fetching Go dependencies ##########"
+go get -v github.com/golang/protobuf/proto
+go get -v github.com/golang/protobuf/protoc-gen-go
+go get -v golang.org/x/net/context
+go get -v google.golang.org/grpc
+go get -v github.com/gemnasium/migrate
+go get -v github.com/sendgrid/sendgrid-go
+
+
 echo "######## Adding to path ##########"
 export PATH=$PATH:/root/protobuf/bin
 
