3c675fb48e9cf6c52d94c24ccb2699a4a46b4b44
PranavShriram <pranavshriram99@gmail.com>
2020-02-14 16:29:19 +0530

    Get money funneling Details

---
+++ models/InspectDetails.go
@@ -0,0 +1,63 @@
+package models
+
+import (
+	"github.com/Sirupsen/logrus"
+	models_pb "github.com/delta/dalal-street-server/proto_build/models"
+)
+
+func (l *InspectDetails) ToProto() *models_pb.InspectDetails {
+	return &models_pb.InspectDetails{
+		Id:               l.UserId,
+		Email:            l.Email,
+		TransactionCount: l.Count,
+		Position:         l.Position,
+		StockSum:         l.StockSum,
+		ReservedstockSum: l.ReservedstockSum,
+	}
+}
+
+type InspectDetails struct {
+	UserId           uint32
+	Email            string
+	Count            uint64
+	Position         uint32
+	StockSum         int64
+	ReservedstockSum int64
+}
+
+type Lrank struct {
+	Position uint32
+}
+
+func GetInspectUserDetails(userID uint32, transType bool) ([]InspectDetails, error) {
+	var l = logger.WithFields(logrus.Fields{
+		"method": "	GetInspectUserDetails",
+		"param_userID": userID,
+	})
+
+	l.Debugf("Attempting to get user details")
+
+	var inspectUserEntries []InspectDetails
+
+	db := getDB()
+	var err error
+	if transType == false {
+		err = db.Raw("SELECT u.id as user_id, u.email as email,COUNT(u.id) as count, sum(t.stockQuantity) as stock_sum, sum(t.reservedStockQuantity) as reservedstock_sum FROM `OrderFills` o, `Bids` b, `Users` u, `Asks` a, `Transactions` t WHERE (o.bidId = b.id AND a.userId = ? AND a.id = o.askId AND b.userID = u.id AND o.transactionId = t.id) GROUP BY u.id ORDER BY COUNT(u.id) DESC LIMIT 10", userID).Scan(&inspectUserEntries).Error
+	} else {
+		err = db.Raw("SELECT u.id as user_id, u.email as email,COUNT(u.id) as count, sum(t.stockQuantity) as stock_sum, sum(t.reservedStockQuantity) as reservedstock_sum FROM `OrderFills` o, `Bids` b, `Users` u, `Asks` a, `Transactions` t WHERE (o.bidId = b.id AND b.userId = ? AND a.id = o.askId AND a.userID = u.id AND o.transactionId = t.id) GROUP BY u.id ORDER BY COUNT(u.id) DESC LIMIT 10", userID).Scan(&inspectUserEntries).Error
+	}
+
+	for i := 0; i < len(inspectUserEntries); i++ {
+		var temp Lrank
+		err = db.Raw("SELECT rank as position FROM Leaderboard WHERE userId = ?", inspectUserEntries[i].UserId).Scan(&temp).Error
+		inspectUserEntries[i].Position = temp.Position
+	}
+
+	if err != nil {
+		l.Errorf("Attempting to get user details")
+		return nil, err
+	}
+
+	return inspectUserEntries, nil
+
+}
