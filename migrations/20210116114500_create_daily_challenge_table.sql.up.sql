4da0af3a04f5752fb4e5a1f9d3353c5f8748472c
siva@2204 <sivaraman26r@gmail.com>
2021-01-17 23:32:56 +0530

    Add UserState migration

--- migrations/20210116114500_create_daily_challenge_table.sql.up.sql
+++ migrations/20210116114500_create_daily_challenge_table.sql.up.sql
@@ -1,10 +1,9 @@
 CREATE TABLE IF NOT EXISTS DailyChallenge(
-    `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
     `marketDate` DATE NOT NULL UNIQUE,
     `challengeType` enum('Cash','NetWorth','StockWorth','SpecificStock'),
     `value` bigint(11) UNSIGNED NOT NULL,
     `stockId` int(11) UNSIGNED DEFAULT NULL,
-    PRIMARY KEY (id),
+    PRIMARY KEY (marketDate),
     FOREIGN KEY (stockId) REFERENCES Stocks(id),
     CONSTRAINT check_daily_challenge CHECK((challengeType='SpecificStock' AND stockId>0 ) OR (challengeType<>'SpecificStock' AND stockId=0))
-) AUTO_INCREMENT=1;
+);
