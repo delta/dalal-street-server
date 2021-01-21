528c43ef6eecd848cdebd54e1f443f658e0fca39
siva@2204 <sivaraman26r@gmail.com>
2021-01-21 23:35:47 +0530

    [Add] AddDailyChallenge rpc

--- models/DailyChallenge.go
+++ models/DailyChallenge.go
@@ -7,11 +7,16 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+var (
+	MarketDay       = 1
+	isChallengeOpen = false
+)
+
 type DailyChallenge struct {
 	Id            uint32 `gorm:"column:id;primary_key;AUTO_INCREMENT" json:"id"`
 	MarketDay     uint32 `gorm:"column:marketDay;not null" json:"market_day"`
 	ChallengeType string `gorm:"column:challengeType;not null" json:"challenge_type"`
-	Value         uint32 `gorm:"column:value;not null" json:"value"`
+	Value         uint64 `gorm:"column:value;not null" json:"value"`
 	StockId       uint32 `gorm:"column:stockIdnot null" json:"stock_id"`
 }
 
@@ -55,4 +60,38 @@ func SaveUsersState() error {
 
 	return nil
 
+}
+
+// add daily challenge to db
+func AddDailyChallenge(value uint64, marketDay uint32, stockId uint32, challengeType string) error {
+	l := logger.WithFields(logrus.Fields{
+		"method":              "AddDailyChallenge",
+		"param_day":           MarketDay,
+		"param_value":         value,
+		"param_stockid":       stockId,
+		"param_challengeType": challengeType,
+	})
+
+	db := getDB()
+
+	l.Infof("Attempting to save daily challenge")
+
+	dailyChallenge := &DailyChallenge{
+		MarketDay:     marketDay,
+		ChallengeType: challengeType,
+		Value:         value,
+	}
+
+	if stockId != 0 {
+		dailyChallenge.StockId = stockId
+	}
+
+	if err := db.Save(dailyChallenge).Error; err != nil {
+		l.Error(err)
+		return err
+	}
+
+	l.Infof("successfully added daily challenge")
+
+	return nil
 }
