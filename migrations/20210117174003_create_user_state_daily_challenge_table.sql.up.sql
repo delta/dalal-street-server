7241ca35619a5b5aa5f9a9ba586c015583f7887f
siva2204 <sivaraman26r@gmail.com>
2021-01-25 20:57:58 +0530

    [Add] saveUserstate functionality

--- migrations/20210117174003_create_user_state_daily_challenge_table.sql.up.sql
+++ migrations/20210117174003_create_user_state_daily_challenge_table.sql.up.sql
@@ -2,8 +2,8 @@ CREATE TABLE IF NOT EXISTS UserState (
      `challengeId` int(11) UNSIGNED NOT NULL,
      `userId` int(11) UNSIGNED NOT NULL,
      `marketDay` int(11) UNSIGNED NOT NULL,
-     `initialValue`bigint(11) UNSIGNED NOT NULL,
-     `finalValue`bigint(11) UNSIGNED DEFAULT NULL,
+     `initialValue`bigint(11) SIGNED NOT NULL,
+     `finalValue`bigint(11) SIGNED DEFAULT NULL,
      `isCompleted`BOOLEAN DEFAULT FALSE,
      FOREIGN KEY (challengeId) REFERENCES DailyChallenge(id),
      FOREIGN KEY (userId) REFERENCES Users(id)
