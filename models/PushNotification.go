382cbcd9cf47a459f727d67acb249930567b94d2
kelpikz <ajithaprasad96@gmail.com>
2021-01-30 18:58:01 +0530

    Feat* : Create payload encryption stuffs

--- models/PushNotification.go
+++ models/PushNotification.go
@@ -1,8 +1,16 @@
 package models
 
 import (
-	"github.com/sirupsen/logrus"
+	"bytes"
+	"crypto/elliptic"
+	"crypto/hmac"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/base64"
 	"encoding/json"
+	"strings"
+
+	"github.com/sirupsen/logrus"
 )
 
 type UserSubscription struct {
@@ -60,4 +68,82 @@ func AddUserSubscription(email string ,data string) error {
 	}
 
 	return nil
+}
+
+/** WEB PUSH LOGIC */
+
+func sendNotification(s *UserSubscription) {
+	curve := elliptic.P256()
+
+	// get dh key for creating a common key
+	dh, err := decodeSubscriptionKey(s.P256dh)
+	if err != nil {
+		// handle error
+	}
+	clientAuthSecret, err := decodeSubscriptionKey(s.Auth)
+
+	serverPrivateKey, x, y, err := elliptic.GenerateKey(curve, rand.Reader)
+	serverPublicKey := elliptic.Marshal(curve, x, y)
+
+	// create a common key with dh
+	sharedX, sharedY := elliptic.Unmarshal(curve, dh)
+
+	if sharedX == nil {
+		// error while generating shared key
+		// public key is not a point on curve
+		// handle error
+	}
+	sx, _ := curve.ScalarMult(sharedX, sharedY, serverPrivateKey)
+	sharedSecret := sx.Bytes()
+
+	// HKDF
+	// pseudo-random-key
+	salt := generateSalt()
+	contentEncryptionKeyInfo := bytes.NewBuffer([]byte("Content-Encoding: aesgcm\x00")) // ends with null, 
+	contentEncryptionKeyInfo.Write(dh)
+	contentEncryptionKeyInfo.Write(serverPublicKey)
+	
+
+	prk := HKDF(clientAuthSecret, sharedSecret, []byte("Content-Encoding: auth\x00"), 32)
+	contentEncryptionKey := HKDF(salt, prk, contentEncryptionKeyInfo.Bytes(), 32)
+
+	nonceInfo := bytes.NewBuffer([]byte("Content-Encoding: nonce\x00")) // ends with null, 
+	nonceInfo.Write(dh)
+	nonceInfo.Write(serverPublicKey)
+	nonce := HKDF(salt, prk, nonceInfo.Bytes(), 12)
+
+}
+
+// helper functions
+
+func decodeSubscriptionKey(key string) ([]byte, error) {
+	buf := bytes.NewBufferString(key)
+	if rem := len(key) % 4; rem != 0 {
+		// add padding
+		buf.WriteString(strings.Repeat("=", 4-rem))
+	}
+
+	return base64.URLEncoding.DecodeString(buf.String())
+}
+
+func HKDF(salt, ikm, info  []byte, length int) []byte {
+	
+	if(length > 32){
+		// handle error
+	}
+	
+	keyHmac := hmac.New(sha256.New, salt)
+	key := keyHmac.Sum(ikm)
+
+	infoHmac := hmac.New(sha256.New, key)
+	infoHmac.Write(info)
+	infoHmac.Write([]byte("\x01"))
+
+	return infoHmac.Sum(nil)[:length]
+}
+
+func generateSalt() ([]byte) {
+	salt := make([]byte, 16)
+	rand.Read(salt)
+	return salt
 }
