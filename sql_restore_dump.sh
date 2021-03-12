c431e5c9d049817abe40e09e62edfe8dadbd1fe1
Sakthi Prasath <ssprasath10@gmail.com>
2021-03-12 15:50:59 +0530

    [Fixes] Some fixes with respect to docker
    * Removed SSL from grpcapi.go and modified log statement to reflec time
    of request
    
    * Added dockerignore
    
    * Removed volume mounts in backend service
    
    * Changed logs path for host
    
    * Added SQL scripts to dump to and restore from db in docker
    
    Co-authored-by: anirudh <anirudh.vs@outlook.com>

---
+++ sql_restore_dump.sh
@@ -0,0 +1,1 @@
+cat /home/deathblade/go/src/github.com/delta/dalal-street-server/dumps/dump-11-03-2021-21-03-07.sql | docker exec -i dalalstreet_db /usr/bin/mysql -u root --password=root dalalstreet_docker
