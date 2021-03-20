24dd6f2a9d644ce21fa09fed9cd1de82fb3efda1
kelpikz <ajithaprasad96@gmail.com>
2021-03-20 20:11:27 +0530

    Fix : Change the push notif icon back to dalal favicon

--- models/PushNotification.go
+++ models/PushNotification.go
@@ -110,7 +110,7 @@ func SendPushNotification(userID uint32, p PushNotification) error {
 	db := getDB()
 
 	if p.LogoUrl == "" {
-		p.LogoUrl = fmt.Sprintf("/public/assets/sed.png")
+		p.LogoUrl = fmt.Sprintf("/public/src/images/dalalfavicon.png")
 	}
 
 	var subscriptions []UserSubscription
