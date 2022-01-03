661778d6260c0f7b7fc09e62a6b69616f8e49179
hedhav <hedhav.haresh@gmail.com>
2022-01-03 23:23:42 +0530

    Changed mysql image to V8 + minor fixes

--- docker-entry.sh
+++ docker-entry.sh
@@ -24,20 +24,22 @@ bash build_proto.sh
 
 # Wait till mysql is accepting connections
 echo "################## Waiting for mysql to accept incoming connections ##################"
-maxtry=3
-while [ $maxtry -gt 0 ]; do
-    nc -z db ${MYSQL_PORT}
-    isopen=$?
-    if [ $isopen -eq 0 ]; then
-        echo "#### No DB :(  #########"
-        break
-    fi
-    maxtry=maxtry-1
-    sleep 1
-done
+# maxtry=3
+# while [ $maxtry -gt 0 ]; do
+#     nc -z db 3306
+#     isopen=$?
+#     if [ $isopen -eq 0 ]; then
+#         echo "#### No DB :(  #########"
+#         break
+#     fi
+#     maxtry=maxtry-1
+#     sleep 1
+# done
+sleep 3
+
 
 echo "######### Running migrations ##########"
-migrate -url "mysql://root:${MYSQL_ROOT_PASSWORD}@tcp(db:${MYSQL_PORT})/dalalstreet_docker" -path ./migrations up
+migrate -path ./migrations -database "mysql://root:${MYSQL_ROOT_PASSWORD}@tcp(db)/dalalstreet_docker" up
 
 echo "################## Starting server ##################"
 go run main.go
@@ -45,5 +47,5 @@ go run main.go
 
 
 echo "########## End of server ###########"
-# sleep 3600
+sleep 3600
 
