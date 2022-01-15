b90c8f8f2fc32306960b344ad174cf6833ca06ff
hedhav <hedhav.haresh@gmail.com>
2022-01-15 15:06:05 +0530

    Updated env stuff

--- scripts/docker-entry.sh
+++ scripts/docker-entry.sh
@@ -8,20 +8,14 @@ bash ./scripts/build_proto.sh
 
 
 echo "################## Waiting for mysql to accept incoming connections ##################"
-declare -i maxtry=3
-while [ $maxtry -gt 0 ]; do
-    nc -z db 3306
-    isopen=$?
-    if [ $isopen -eq 0 ]; then
-        break
-    fi
-    maxtry=${maxtry}-1
-    sleep 1
+until nc -z -v -w30 db 3306
+do
+  echo "     Waiting for database connection    "
+  sleep 3
 done
 
-
 echo "######### Running migrations ##########"
-migrate -path ./migrations -database "mysql://root:${MYSQL_ROOT_PASSWORD}@tcp(db)/dalalstreet_docker" up
+migrate -path ./migrations -database "mysql://root:${DB_PASS}@tcp(db)/${DB_NAME}" up
 
 
 echo "################## Starting server ##################"
