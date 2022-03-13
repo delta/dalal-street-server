567c0c99fbac50a860bba4defe957edcbbdf8187
siva2204 <sivaraman26r@gmail.com>
2022-03-13 23:22:44 +0530

    [fix] images

--- httpapi/mux.go
+++ httpapi/mux.go
@@ -13,12 +13,5 @@ func Init() {
 	HttpMux.HandleFunc("/verify", handleVerification)
 
 	//serve public dir
-	HttpMux.Handle("/static/", http.StripPrefix("/static/", http.HandlerFunc(func(resp http.ResponseWriter, req *http.Request) {
-
-		resp.Header().Add("Access-Control-Allow-Origin", "*")
-		resp.Header().Add("Access-Control-Allow-Methods", "*")
-		resp.Header().Add("Access-Control-Allow-Headers", "Content-Type,x-grpc-web,sessionid,x-user-agent")
-		resp.Header().Add("Access-Control-Max-Age", "600")
-		http.FileServer(http.Dir("./public"))
-	})))
+	HttpMux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./public"))))
 }
